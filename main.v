50445923df60d214fc70bd94bdbd1551b45bd232
Shiishii <flavien.cadet@nocturlab.fr>
2020-05-16 19:38:50 +0200

    change to vui

--- main.v
+++ main.v
@@ -1,49 +1,109 @@
-module vi_v
-
-import gg2 as gg
+import ui
 import gx
 import os
 
+// Some parameters
 const (
-	win_width = 600
-	win_height = 300
+	app_title = 'Vi-v'
+	win_width = 700
+	win_height = 385
+	nr_cols = 4
+	cell_height = 25
+	cell_width = 100
+	table_width = cell_width * nr_cols
 )
 
+// State can save the actual window state for futher restoration. This is the app
 struct App {
 mut:
-	gg &gg.GG
+	window     &ui.Window
+	label      &ui.Label
+	started    bool
 }
 
+// Vi-v entrypoint
 fn main() {
-	mut app := &App{}
-	app.gg = gg.new_context({
-		bg_color: gx.white
+	// Instantiate the app
+	mut app := &App{
+		//some var to save that can restore the actual state of window
+    label: app_title
+	}
+
+	create(app)
+	
+	ui.run(app.window)
+}
+
+fn create(app mut &App) {
+	// Open the window
+	window := ui.window({
 		width: win_width
 		height: win_height
-		use_ortho: true // This is needed for 2D drawing
-		create_window: true
-		window_title: 'Empty window'
-		frame_fn: frame
-		user_data: app
-	})
-	app.gg.run()
+		state: app
+		title: app_title
+	}, [
+		ui.row({
+			stretch: true
+			margin: {top:10,left:10,right:10,bottom:10}
+		}, [
+			ui.column({
+				width: 200
+				spacing: 13
+			}, [
+				// left column display
+			])
+			ui.column({
+				stretch: true
+				alignment: .right
+			},[
+				// mid column dislay
+				ui.canvas(
+					height: 275
+					draw_fn: canvas_draw
+				)
+			])
+			ui.column({
+				stretch: true
+				alignment: .right
+			},[
+				// left column dislay
+			])
+		]),
+		ui.menu(
+			items: [
+				ui.MenuItem{'Delete all users', menu_click},
+				ui.MenuItem{'Export users', menu_click},
+				ui.MenuItem{'Exit', menu_click},
+			]
+		)
+	])
+	app.window = window
 }
 
-fn frame(user_data voidptr) {
-	mut app := &App(user_data)
-	mut gg := app.gg
-	gg.begin()
-	if gg.fons == 0 {
-		gg.init_font()
-	}
-	app.draw()
-	C.sfons_flush(gg.fons)
-	gg.end()
+
+fn menu_click() {
+  ui.message_box('Menu clicked')
 }
 
-fn (app &App) draw() {
-	app.gg.draw_text_def(200,20, 'hello world!')
-	app.gg.draw_text_def(300,300, 'привет')
-	app.gg.draw_rect(10, 10, 100, 30, gx.blue)
-	app.gg.draw_empty_rect(10, 150, 80, 40, gx.green)
+fn btn_help_click(a, b voidptr) {
+  ui.message_box('Built with V UI')
+}
+
+/*
+fn (app mut App) btn_add_click(b &Button) {
+}
+*/
+
+fn btn_add_click(mut app App, x voidptr) {
+	
+}
+
+fn canvas_draw(app &App) {
+	gg := app.window.ui.gg
+	x := 240
+	gg.draw_rect(x - 20, 0, table_width + 100, 800, gx.white)
+	y := 20 * cell_height
+	// Outer border
+	gg.draw_empty_rect(x, y, table_width, cell_height, gx.Gray)
+	gg.draw_empty_rect(x - 20, y, table_width, cell_height, gx.Gray)
 }
---
+++ v.mod
@@ -0,0 +1,10 @@
+#V Project#
+
+Module {
+	name: 'vi-v',
+	description: 'Vi-v is a visual scripting UI for vlang. It allow anyone to create their own programs easly where low programmation skills is needed.',
+  version: '0.0.1',
+  dependencies: [
+		ui
+	]
+}
